3bfa456a4b0cee5015e8f803bd0930e96f9e6578
Stijn De Pestel <stijn.depestel@intec.ugent.be>
2016-02-25 23:46:16 +0100

    Skeleton for ReplayTest. Name changes in POM and removed dyamand parent.

---
+++ src/test/java/com/stijndepestel/thesis/capturereplay/ReplayTest.java
@@ -0,0 +1,43 @@
+package com.stijndepestel.thesis.capturereplay;
+
+import org.json.JSONObject;
+import org.junit.Before;
+
+/**
+ * Test for the replay functionality.
+ *
+ * @author sjdpeste
+ *
+ */
+public class ReplayTest {
+
+    /**
+     * The replay object.
+     */
+    private Replay<TestEvent> replay;
+
+    /**
+     * Create new capture object before each test.
+     */
+    @Before
+    public void before() {
+        this.replay = new Replay<>(TestHelper::deserialize,
+                () -> new JSONObject("{}"), event -> {
+                    return null;
+                });
+    }
+
+    // @Test
+    // public void eventsCapturedTest() throws InterruptedException
+    // @Test(expected = IllegalStateException.class)
+    // public void exceptionOnCaptureOnInvalidStateWhenNotStartedTest()
+    // @Test(expected = IllegalStateException.class)
+    // public void exceptionOnCaptureOnInvalidStateWhenStoppedTest()
+    // @Test(expected = IllegalStateException.class)
+    // public void exceptionOnStoppingCaptureOnInvalidStateTest()
+    // @Test(expected = IllegalStateException.class)
+    // public void exceptionOnPersistingOnInvalidCreatedStateTest()
+    // @Test(expected = IllegalStateException.class)
+    // public void exceptionOnPersistingOnInvalidCapturingStateTest()
+
+}
