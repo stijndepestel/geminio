caf74f4eb446451865e770436177afdec6628e00
Stijn De Pestel <stijn.depestel@intec.ugent.be>
2016-03-18 14:50:20 +0100

    Changed event catcher from Function<T,?> to Consumer<T>.

--- src/main/java/com/stijndepestel/thesis/capturereplay/Replay.java
+++ src/main/java/com/stijndepestel/thesis/capturereplay/Replay.java
@@ -1,6 +1,7 @@
 package com.stijndepestel.thesis.capturereplay;
 
 import java.util.PriorityQueue;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -11,162 +12,164 @@ import org.json.JSONObject;
  * Defines the methods to replay captured events.
  *
  * @author sjdpeste
- * 
+ *
  * @param <T>
  *            The type of events to replay.
  *
  */
 public final class Replay<T> {
 
-	/**
-	 * Treshold defining the error margin of the sleep functionality.
-	 */
-	private static final long TRESHOLD = 500L;
+    /**
+     * The error message for the IllegalStateExceptions thrown in this object.
+     */
+    private static final String ERROR_MESSAGE = "Object is not in the right state.";
 
-	/**
-	 * Deserializer for the events that will be replayed.
-	 */
-	private final Function<JSONObject, T> deserializer;
+    /**
+     * Treshold defining the error margin of the sleep functionality.
+     */
+    private static final long TRESHOLD = 200L;
 
-	/**
-	 * Supplier of the JSONObject. Can f.e. be loaded from file.
-	 *
-	 * @see FilePersister
-	 */
-	private final Supplier<JSONObject> loader;
+    /**
+     * Deserializer for the events that will be replayed.
+     */
+    private final Function<JSONObject, T> deserializer;
 
-	/**
-	 * Catcher of the events that will be replayed.
-	 */
-	private final Function<T, ?> eventCatcher;
+    /**
+     * Supplier of the JSONObject. Can f.e. be loaded from file.
+     *
+     * @see FilePersister
+     */
+    private final Supplier<JSONObject> loader;
 
-	/**
-	 * Priority queue that holds the events that still have to be replayed.
-	 * Ordered by the natural ordering (Comparable) of Wrapper.
-	 */
-	private final PriorityQueue<Wrapper<T>> queue;
+    /**
+     * Catcher of the events that will be replayed.
+     */
+    private final Consumer<T> eventCatcher;
 
-	/**
-	 * The current state of the replay object.
-	 */
-	private State currentState;
+    /**
+     * Priority queue that holds the events that still have to be replayed.
+     * Ordered by the natural ordering (Comparable) of Wrapper.
+     */
+    private final PriorityQueue<Wrapper<T>> queue;
 
-	/**
-	 * Timestamp of when replay started.
-	 */
-	private long replayStart;
+    /**
+     * The current state of the replay object.
+     */
+    private State currentState;
 
-	/**
-	 * Create new Replay object.
-	 *
-	 * @param deserializer
-	 *            Deserializer for the events (converts from JSONObject to T (==
-	 *            event type)).
-	 * @param loader
-	 *            Supplies the JSONObject containing all the previously captured
-	 *            events.
-	 * @param eventCatcher
-	 *            Catcher of the event that will be thrown by the replay. Return
-	 *            type of this function can be anything (including Void) and
-	 *            will be ignored.
-	 */
-	public Replay(final Function<JSONObject, T> deserializer,
-			final Supplier<JSONObject> loader, final Function<T, ?> eventCatcher) {
-		this.deserializer = deserializer;
-		this.loader = loader;
-		this.eventCatcher = eventCatcher;
-		this.queue = new PriorityQueue<>();
-		this.currentState = State.CREATED;
-	}
+    /**
+     * Timestamp of when replay started.
+     */
+    private long replayStart;
 
-	/**
-	 * Load the events.
-	 *
-	 * @return Reference to this instance.
-	 */
-	public Replay<T> load() {
-		if (this.currentState != State.CREATED) {
-			throw new IllegalArgumentException(
-					"Object is not in correct state to start the replay.");
-		}
-		// Get the json
-		final JSONArray jsonarr = this.loader.get().getJSONArray(
-				JSONNames.JSON_EVENTS);
-		jsonarr.forEach(json -> this.queue.add(new Wrapper<>((JSONObject) json,
-				this.deserializer)));
-		this.currentState = State.LOADED;
-		return this;
-	}
+    /**
+     * Create new Replay object.
+     *
+     * @param deserializer
+     *            Deserializer for the events (converts from JSONObject to T (==
+     *            event type)).
+     * @param loader
+     *            Supplies the JSONObject containing all the previously captured
+     *            events.
+     * @param eventCatcher
+     *            Catcher of the event that will be thrown by the replay. Return
+     *            type of this function can be anything (including Void) and
+     *            will be ignored.
+     */
+    public Replay(final Function<JSONObject, T> deserializer,
+            final Supplier<JSONObject> loader, final Consumer<T> eventCatcher) {
+        this.deserializer = deserializer;
+        this.loader = loader;
+        this.eventCatcher = eventCatcher;
+        this.queue = new PriorityQueue<>();
+        this.currentState = State.CREATED;
+    }
 
-	/**
-	 * Start the replay, will load and deserialize all events and start
-	 * replaying them.
-	 */
-	public void startReplay() {
-		if (this.currentState != State.LOADED) {
-			throw new IllegalArgumentException(
-					"Object is not in correct state to start the replay.");
-		}
-		this.currentState = State.REPLAYING;
-		this.replayStart = System.currentTimeMillis();
-		// start replay
-		this.replay();
-	}
+    /**
+     * Load the events.
+     *
+     * @return Reference to this instance.
+     */
+    public Replay<T> load() {
+        if (this.currentState != State.CREATED) {
+            throw new IllegalStateException(Replay.ERROR_MESSAGE);
+        }
+        // Get the json
+        final JSONArray jsonarr = this.loader.get().getJSONArray(
+                JSONNames.JSON_EVENTS);
+        jsonarr.forEach(json -> this.queue.add(new Wrapper<>((JSONObject) json,
+                this.deserializer)));
+        this.currentState = State.LOADED;
+        return this;
+    }
 
-	/**
-	 * Recursive replay function.
-	 */
-	private void replay() {
-		if (this.currentState != State.REPLAYING) {
-			throw new IllegalArgumentException(
-					"Object is not in replaying state.");
-		}
-		if (this.queue.isEmpty()) {
-			// end of replay
-			this.currentState = State.STOPPED;
-			return;
-		}
-		final long nextReplayTime = this.queue.peek().getRelativeTimestamp();
-		final long timerunning = System.currentTimeMillis() - this.replayStart;
-		final long toSleep = nextReplayTime - timerunning;
-		// if within treshold
-		if (toSleep < Replay.TRESHOLD) {
-			// throw event
-			this.eventCatcher.apply(this.queue.poll().getEvent());
-		} else {
-			try {
-				Thread.sleep(toSleep);
-			} catch (final InterruptedException e) {
-				// Ignore exception
-			}
-		}
-		// Recursion
-		this.replay();
-	}
+    /**
+     * Start the replay, will load and deserialize all events and start
+     * replaying them.
+     */
+    public void startReplay() {
+        if (this.currentState != State.LOADED) {
+            throw new IllegalStateException(Replay.ERROR_MESSAGE);
+        }
+        this.currentState = State.REPLAYING;
+        this.replayStart = System.currentTimeMillis();
+        // start replay
+        this.replay();
+    }
 
-	/**
-	 * Definitions of the possible states of a Replay object.
-	 *
-	 * @author sjdpeste
-	 *
-	 */
-	private enum State {
-		/**
-		 * The object has been created but has not yet started the replay.
-		 */
-		CREATED,
-		/**
-		 * The object has loaded the events for replaying.
-		 */
-		LOADED,
-		/**
-		 * The object is in replay mode, it is throwing.
-		 */
-		REPLAYING,
-		/**
-		 * The object has finished replaying the events or was interrupted.
-		 */
-		STOPPED;
+    /**
+     * Recursive replay function.
+     */
+    private void replay() {
+        if (this.currentState != State.REPLAYING) {
+            throw new IllegalStateException(Replay.ERROR_MESSAGE);
+        }
+        if (this.queue.isEmpty()) {
+            // end of replay
+            this.currentState = State.STOPPED;
+            return;
+        }
+        final long nextReplayTime = this.queue.peek().getRelativeTimestamp();
+        final long timerunning = System.currentTimeMillis() - this.replayStart;
+        final long toSleep = nextReplayTime - timerunning;
+        // if within treshold
+        if (toSleep < Replay.TRESHOLD) {
+            // throw event
+            this.eventCatcher.accept(this.queue.poll().getEvent());
+        } else {
+            try {
+                Thread.sleep(toSleep);
+            } catch (final InterruptedException e) {
+                // Ignore exception
+            }
+        }
+        // Recursion
+        this.replay();
+    }
 
-	}
+    /**
+     * Definitions of the possible states of a Replay object.
+     *
+     * @author sjdpeste
+     *
+     */
+    private enum State {
+        /**
+         * The object has been created but has not yet started the replay.
+         */
+        CREATED,
+        /**
+         * The object has loaded the events for replaying.
+         */
+        LOADED,
+        /**
+         * The object is in replay mode, it is throwing.
+         */
+        REPLAYING,
+        /**
+         * The object has finished replaying the events or was interrupted.
+         */
+        STOPPED;
+
+    }
 }
